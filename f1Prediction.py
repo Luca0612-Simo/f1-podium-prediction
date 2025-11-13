98f6f33ddcb77d34506f5a85288195d3e4c8fabc
Luca <lucasimonetti0612@gmail.com>
2025-11-13 18:48:15 -0300

    feat: search the best Hiperparams

--- f1Prediction.py
+++ f1Prediction.py
@@ -4,8 +4,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 try:
-    results = pd.read_csv('results.csv')
-    races = pd.read_csv('races.csv')
+    results = pd.read_csv('archive/results.csv')
+    races = pd.read_csv('archive/races.csv')
 except FileNotFoundError:
     print("No se encuentran los archivos")
     exit()
@@ -23,9 +23,20 @@ data['podium'] = data['positionOrder'].apply(lambda x: 1 if 1 <= x <= 3 else 0)
 
 data = data.drop('positionOrder', axis=1)
 
-print("Así quedó el dataset listo para el modelo:")
+print("Dataset listo para el modelo:")
 print(data.head())
 print(f"\nTotal de carreras a analizar: {len(data)}")
 
 X = data.drop('podium', axis=1)
 y = data['podium']
+
+param_grid = {
+    'min_samples_split': [2, 10, 20, 50],
+    'max_depth': [3, 4, 5, 7, None]
+}
+
+grid = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid, cv=5, scoring='accuracy')
+grid.fit(X, y)
+
+print("Mejores parámetros encontrados:", grid.best_params_)
+print("Mejor score", grid.best_score_)
